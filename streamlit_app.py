4bbf6271e4970d822ecb80cfc47ac5e1e06dc9a6
Rahul Sonone <rahulsonone1234@gmail.com>
2025-03-04 17:38:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ st.write("The name on your smoothie will be:", name_on_order)
 
 
 # Get the current credentials
-cnx=st.connection("Snowflake")
+cnx=st.connection("snowflake")
 session=cnx.session()
 # session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'))
